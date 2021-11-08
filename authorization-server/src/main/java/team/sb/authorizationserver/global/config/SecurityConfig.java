e310a9c6d81f87610e73451d879a9625694a679d
김범진 <sdpthf@naver.com>
2021-11-08 19:28:55 +0900

    ⚡️ :: SecurityConfig

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/SecurityConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/SecurityConfig.java
@@ -1,43 +1,34 @@
 package team.sb.authorizationserver.global.config;
 
-import org.springframework.context.annotation.Bean;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+    private final CustomAuthenticationProvider authenticationProvider;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("1234")
-                .roles("USER");
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
-    protected void configure(HttpSecurity security) throws Exception {
-        security
+    protected void configure(HttpSecurity http) throws Exception {
+        http
                 .csrf().disable()
-                .headers().frameOptions().disable()
-                .and()
-                .authorizeRequests().antMatchers("/oauth/**", "/oauth2/**").permitAll()
-                .and()
+
                 .formLogin().and()
-                .httpBasic();
+                .httpBasic()
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .antMatchers("/oauth2/**").permitAll();
     }
 
 }
