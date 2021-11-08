7b45c10844b8b6714a424a040b80ce689d593a3d
김범진 <sdpthf@naver.com>
2021-11-08 21:46:33 +0900

    ⚡️ :: signKey

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthorizationConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthorizationConfig.java
@@ -9,45 +9,27 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import team.sb.authorizationserver.global.security.CustomUserDetailsService;
 
 import javax.sql.DataSource;
 
-@Configuration
 @RequiredArgsConstructor
+@Configuration
 @EnableAuthorizationServer
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("security.oauth2.jwt.signkey")
+    private String signKey;
 
     private final DataSource dataSource;
     private final PasswordEncoder passwordEncoder;
     private final CustomUserDetailsService userDetailService;
 
-    @Value("security.oauth2.jwt.signkey")
-    private String signKey;
-
-//    @Bean
-//    public void configure(ClientDetailsServiceConfigurer config) throws Exception {
-//        config.inMemory()
-//                .withClient("testClientId")
-//                .secret("testSecret")
-//                .redirectUris("http://localhost:8080/oauth2/callback")
-//                .scopes("read", "write")
-//                .accessTokenValiditySeconds(30000);
-//    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) {
-        security.tokenKeyAccess("permitAll()")
-                .checkTokenAccess("isAuthenticated()") //allow check token
-
-                .allowFormAuthenticationForClients();
     }
 
     @Override
