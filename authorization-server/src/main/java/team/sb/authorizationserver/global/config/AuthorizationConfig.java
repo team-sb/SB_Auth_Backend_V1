eb4ed04c2db997f07333217109804671dee0d0bb
김범진 <sdpthf@naver.com>
2021-11-08 19:28:46 +0900

    ⚡️ :: AuthorizationConfig

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthorizationConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthorizationConfig.java
@@ -1,12 +1,17 @@
 package team.sb.authorizationserver.global.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import team.sb.authorizationserver.global.security.CustomUserDetailsService;
 
 import javax.sql.DataSource;
 
@@ -15,28 +20,47 @@ import javax.sql.DataSource;
 @EnableAuthorizationServer
 public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
     private final DataSource dataSource;
+    private final PasswordEncoder passwordEncoder;
+    private final CustomUserDetailsService userDetailService;
 
-    public void configure(ClientDetailsServiceConfigurer config) throws Exception {
-        config.inMemory()
-                .withClient("testClientId")
-                .secret("testSecret")
-                .redirectUris("http://localhost:8080/oauth2/callback")
-                .scopes("read", "write")
-                .accessTokenValiditySeconds(30000);
+    @Value("security.oauth2.jwt.signkey")
+    private String signKey;
+
+//    @Bean
+//    public void configure(ClientDetailsServiceConfigurer config) throws Exception {
+//        config.inMemory()
+//                .withClient("testClientId")
+//                .secret("testSecret")
+//                .redirectUris("http://localhost:8080/oauth2/callback")
+//                .scopes("read", "write")
+//                .accessTokenValiditySeconds(30000);
+//    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
     }
 
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) {
         security.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()") //allow check token
+
                 .allowFormAuthenticationForClients();
     }
 
-//    @Override
-//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-//        clients.jdbc(dataSource).passwordEncoder(passwordEncoder);
-//    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
+    }
 
 }
