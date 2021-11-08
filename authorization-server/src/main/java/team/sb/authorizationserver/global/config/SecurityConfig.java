4bcd84c3a1899510e227dd1332dfc24147c9fd96
김범진 <sdpthf@naver.com>
2021-11-08 12:52:33 +0900

    📑 :: SecurityConfig

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package team.sb.authorizationserver.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("1234")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().antMatchers("/oauth/**", "/oauth2/**").permitAll()
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+}
