746be6d6b28c14bc59b1a8d46346c0e87b2e0230
김범진 <sdpthf@naver.com>
2021-11-08 19:28:19 +0900

    📑 :: CustomAuthenticationProvider

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/CustomAuthenticationProvider.java
@@ -0,0 +1,49 @@
+package team.sb.authorizationserver.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import team.sb.authorizationserver.domain.user.entity.User;
+import team.sb.authorizationserver.domain.user.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) {
+
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        userRepository.save(User.builder()
+                .name("user")
+                .uid("user")
+                .password(passwordEncoder.encode("1234"))
+                .build());
+
+        User user = userRepository.findByUid(name)
+                .orElseThrow(() -> new UsernameNotFoundException("user is not exists"));
+
+        if (!passwordEncoder.matches(password, user.getPassword()))
+            throw new BadCredentialsException("password is not valid");
+
+        return new UsernamePasswordAuthenticationToken(name, password, user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(
+                UsernamePasswordAuthenticationToken.class);
+    }
+
+}
