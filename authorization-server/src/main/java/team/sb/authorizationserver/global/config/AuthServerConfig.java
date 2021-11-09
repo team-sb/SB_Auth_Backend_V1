78b1274a63ce51bf2ff2a9d4c86cb39d6e3bbfe5
김범진 <sdpthf@naver.com>
2021-11-09 22:19:35 +0900

    ⚡️ :: AuthServerConfig

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import team.sb.authorizationserver.global.security.CustomUserDetailsService;
 
 import javax.sql.DataSource;
@@ -17,7 +20,7 @@ import javax.sql.DataSource;
 @RequiredArgsConstructor
 @Configuration
 @EnableAuthorizationServer
-public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Value("security.oauth2.jwt.signkey")
     private String signKey;
@@ -33,15 +36,29 @@ public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
     }
 
     @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }
+
+
+    @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         super.configure(endpoints);
-        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+        endpoints
+                .accessTokenConverter(jwtAccessTokenConverter())
+                .userDetailsService(userDetailService);
     }
+
 
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+                new FileSystemResource("src/main/resources/oauth2jwt.jks"), "oauth2jwtpass".toCharArray()
+        );
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("oauth2jwt"));
         return converter;
     }
 
