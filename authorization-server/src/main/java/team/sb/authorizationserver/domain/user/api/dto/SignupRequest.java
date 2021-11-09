d11ce4c582618604ac8af4188334976045fd5824
김범진 <sdpthf@naver.com>
2021-11-09 09:24:29 +0900

    📑 :: SignupRequest

---
+++ authorization-server/src/main/java/team/sb/authorizationserver/domain/user/api/dto/SignupRequest.java
@@ -0,0 +1,15 @@
+package team.sb.authorizationserver.domain.user.api.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SignupRequest {
+
+    private String uid;
+    private String password;
+    private String name;
+    private String provider;
+
+}
