b2dfa4e41951b888e04d18fa09552d3f8c671c4f
김범진 <sdpthf@naver.com>
2021-11-15 17:27:34 +0900

    ⚡️ :: tokenStore & approvalStoreDisabled

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
@@ -4,20 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import team.sb.authorizationserver.global.security.CustomUserDetailsService;
 
 import javax.sql.DataSource;
@@ -51,25 +46,27 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
         super.configure(endpoints);
         endpoints
                 .accessTokenConverter(jwtAccessTokenConverter())
-                .userDetailsService(userDetailService);
-    }
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
-                new ClassPathResource("src/main/resources/sbtoken.jks"), "sbpass".toCharArray()
-        );
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("sbauth"));
-        return converter;
+                .tokenStore(tokenStore())
+                .userDetailsService(userDetailService)
+                .approvalStoreDisabled();
     }
 
 //    @Bean
 //    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(
+//                new ClassPathResource("src/main/resources/sbtoken.jks"), "sbpass".toCharArray()
+//        );
 //        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-//        converter.setSigningKey(signKey);
+//        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("sbauth"));
 //        return converter;
 //    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
+    }
 
     @Bean
     public TokenStore tokenStore() {
