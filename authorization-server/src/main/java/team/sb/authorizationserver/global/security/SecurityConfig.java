8721ae1760ca24c633eb8dd1686ff9f401b7c24f
김범진 <sdpthf@naver.com>
2021-11-15 17:23:36 +0900

    ⚡️ :: userDetailsService 등록

--- authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package team.sb.authorizationserver.global.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final CustomUserDetailsService userDetailsService;
     private final CustomAuthenticationProvider authenticationProvider;
 
     @Bean
@@ -30,7 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
+        auth
+                .authenticationProvider(authenticationProvider)
+                .userDetailsService(userDetailsService);
     }
 
     @Override
