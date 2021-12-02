9b609160f87b9a0a9947a11aa276130f1249f139
김범진 <sdpthf@naver.com>
2021-12-02 19:30:52 +0900

    ⚡ :: User

--- resource-server/src/main/java/team/sb/resourceserver/domain/user/entity/User.java
+++ resource-server/src/main/java/team/sb/resourceserver/domain/user/entity/User.java
@@ -2,15 +2,20 @@ package team.sb.resourceserver.domain.user.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,5 +30,40 @@ public class User {
 
     @Column(nullable = false, columnDefinition = "char(5)")
     private String name;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(this.uid));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.uid;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
