c3748cab9ccd74f24da50a158f3de0fe2e1eea94
김범진 <sdpthf@naver.com>
2021-12-02 16:21:45 +0900

    📑 :: CustomUserDetailsService

---
+++ resource-server/src/main/java/team/sb/resourceserver/global/security/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package team.sb.resourceserver.global.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import team.sb.resourceserver.domain.user.entity.User;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority(user.getUid()));
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUid();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
