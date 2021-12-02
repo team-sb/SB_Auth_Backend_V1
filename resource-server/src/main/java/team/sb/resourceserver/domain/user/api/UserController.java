880246bc69d06c7b9a8eeb61ae917ea9d613ee93
김범진 <sdpthf@naver.com>
2021-12-02 19:31:56 +0900

    ⚡ ::  유저 정보 조회

--- resource-server/src/main/java/team/sb/resourceserver/domain/user/api/UserController.java
+++ resource-server/src/main/java/team/sb/resourceserver/domain/user/api/UserController.java
@@ -1,20 +1,31 @@
 package team.sb.resourceserver.domain.user.api;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.sb.resourceserver.domain.user.entity.User;
+import team.sb.resourceserver.domain.user.repository.UserRepository;
 
+@RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
 public class UserController {
 
+    private final UserRepository userRepository;
+
     @GetMapping("/user")
-    public ResponseEntity<?> userInfo() {
+    public ResponseEntity<?> getUserInfo() {
+        return ResponseEntity.ok(getCurrentUser().getName());
+    }
+
+    public User getCurrentUser() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        return ResponseEntity.ok(principal);
+        return userRepository.findByUid(principal.toString())
+                .orElseThrow(() -> new RuntimeException("user not found"));
     }
 
 }
