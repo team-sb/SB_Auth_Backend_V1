ccebb350e2412c4427d85b627af354e2ec061d02
김범진 <sdpthf@naver.com>
2021-12-02 14:57:57 +0900

    🐛 :: User로 변환안됨

--- resource-server/src/main/java/team/sb/resourceserver/domain/user/api/UserController.java
+++ resource-server/src/main/java/team/sb/resourceserver/domain/user/api/UserController.java
@@ -4,12 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import team.sb.resourceserver.domain.user.entity.User;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
 
 @RequestMapping("/api")
 @RestController
@@ -17,10 +12,9 @@ public class UserController {
 
     @GetMapping("/user")
     public ResponseEntity<?> userInfo() {
-        User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        System.out.println(principal.getUid());
-        System.out.println(principal.getName());
+        System.out.println(principal);
 
         return ResponseEntity.ok(principal);
     }
