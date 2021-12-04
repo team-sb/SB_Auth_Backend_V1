986b71fca124d6f1b756ae2e0a149b36e8fb04ab
김범진 <sdpthf@naver.com>
2021-12-04 16:48:22 +0900

    ⚰ :: CustomJwtAccessTokenConverter

--- authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
+++ authorization-server/src/main/java/team/sb/authorizationserver/global/config/AuthServerConfig.java
@@ -15,7 +15,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-//import team.sb.authorizationserver.global.security.CustomJwtAccessTokenConverter;
 import team.sb.authorizationserver.global.security.CustomUserDetailsService;
 
 import javax.sql.DataSource;
