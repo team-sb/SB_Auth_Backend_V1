4fe945e642666bc7c426b79bbc25803f6d28c787
김범진 <sdpthf@naver.com>
2021-12-14 22:17:39 +0900

    📑 :: GlobalExceptionHandler

---
+++ resource-server/src/main/java/team/sb/resourceserver/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package team.sb.resourceserver.global.error;
+
+import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import team.sb.authorizationserver.global.error.exception.SBException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(SBException.class)
+    public ResponseEntity<ErrorResponse> globalExceptionHandler(SBException e) {
+        final ErrorCode errorCode = e.getErrorCode();
+
+        return new ResponseEntity<>(new ErrorResponse(errorCode.getStatus(), errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatus()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validExceptionHandler(MethodArgumentNotValidException e) {
+        return new ResponseEntity<>(new ErrorResponse(400, e.getBindingResult().getAllErrors().get(0).getDefaultMessage()),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<ErrorResponse>
+    propertyReferenceExceptionHandler(PropertyReferenceException e) {
+        return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+}
